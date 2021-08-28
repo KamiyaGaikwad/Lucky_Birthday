d10e8680ab5767e00d44923731d77bcdf7af6fb3
Kamiya <kamitarg88@gmail.com>
2021-08-28 18:07:45 +0530

    ex: 01 calculate the sum of digits of the date

--- index.js
+++ index.js
@@ -5,5 +5,27 @@ var checkButton = document.querySelector("#check-button");
 checkButton.addEventListener('click',takeInputForOperation);
 
 function takeInputForOperation(){
-    console.log(DoB.value,luckyNumber.value);
+    var bday = DoB.value;
+    bday = bday.replaceAll("-","");
+    sum = sumTheNumber(bday);
+    // alert(sum);
 }
+
+function sumTheNumber(bday){
+    let sum=0;
+    for(let i=0;i<bday.length;i++){
+        sum = sum + Number(bday.charAt(i));
+        console.log(sum)
+    }
+    return sum;
+}
+
+
+
+
+
+
+
+
+
+//bonus- try to do replaceAll with manual coding
